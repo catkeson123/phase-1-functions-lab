60658b29b2b1f132d026c5c46bcddb20c8322aa2
Calvin Atkeson <calvin.atkeson@gmail.com>
2023-02-01 10:44:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+  return Math.abs(endBlock - startBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+  const feet = distanceTravelledInFeet(startBlock, endBlock);
+
+  if (feet < 400) {
+    return 0;
+  } else if (feet >= 400 && feet < 2000) {
+    return (feet - 400) * 0.02;
+  } else if (feet >= 2000 && feet < 2500) {
+    return 25;
+  } else if (feet >= 2500) {
+    return "cannot travel that far";
+  }
+}
